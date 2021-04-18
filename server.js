5693082eb959ebefec76ae542a535fe4463ea9ce
VSLG <vlopez_jv@hotmail.com>
2021-04-17 17:56:37 -0700

    added npm pkg, server file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// Dependencies
+// ===========================================================
+const express = require('express');
+
+const app = express();
+const PORT = 3001;
+
+
+// Listener
+// ===========================================================
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+  });
+  
